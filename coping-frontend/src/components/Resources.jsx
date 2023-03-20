f570a1da42b73341a57a9f88bd67a3a5f206ac83
Duncan <duncanwoodpro@gmail.com>
2023-03-20 19:07:26 -0400

    working on resources component

--- coping-frontend/src/components/Resources.jsx
+++ coping-frontend/src/components/Resources.jsx
@@ -9,13 +9,13 @@ export default function Resources() {
   const [resources, setResources] = useState([]);
   let navigate = useNavigate();
 
-  let arrayTypes = [
-    "type_meditation",
-    "type_movement",
-    "type_distraction",
-    "type_grounding",
-    "type_affirmation",
-  ];
+  // let arrayTypes = [
+  //   "type_meditation",
+  //   "type_movement",
+  //   "type_distraction",
+  //   "type_grounding",
+  //   "type_affirmation",
+  // ];
 
   useEffect(() => {
     const handleResources = async () => {
@@ -26,19 +26,64 @@ export default function Resources() {
   }, []);
 
   const handleFilteredResources = () => {
-    return resources.filter((resource) => resource.user_id === 1)
-    console.log(filteredResources)
+    return resources.filter((resource) => resource.user_id === 1);
+  };
+  const filteredResources =
+    !user && !authenticated ? handleFilteredResources() : resources;
+
+  let authenticatedResources;
+  if (user) {
+    authenticatedResources = (
+      <div className="user_feed">
+        <div className="logged_resources_header">
+          <h1>POPULAR RESOURCES</h1>
+        </div>
+        {filteredResources.map(
+          (resource) => (
+            console.log(filteredResources),
+            (
+              <div className="resource-card" key={resource.id}>
+                <h3>{resource.title}</h3>
+                <h5>by: {resource.User.username}</h5>
+              </div>
+            )
+          )
+        )}
+      </div>
+    );
   }
-  const filteredResources = !user && !authenticated ? handleFilteredResources() : resources;
+
+  const publicResources = (
+    <div className="user_feed">
+      <div className="unlogged-resources-header">
+        <h1>POPULAR RESOURCES</h1>
+        <h2>BY COPING CORNER HQ</h2>
+      </div>
+      <div className="please-register">
+        <h5>
+          TO VIEW OUR WHOLE LIBRARY OF RESOURCES, PLEASE REGISTER A
+          <span style={{ color: "cyan" }}> FREE ACCOUNT </span>
+          OR LOG IN NOW!
+        </h5>
+      </div>
+      {filteredResources.map(
+        (resource) => (
+          console.log(filteredResources),
+          (
+            <div className="resource-card" key={resource.id}>
+              <h3>{resource.title}</h3>
+              <h5>by: {resource.User.username}</h5>
+            </div>
+          )
+        )
+      )}
+    </div>
+  );
 
   return (
-    <div className="user_feed">
-      {filteredResources.map((resource) => (
-        <div className="resource-card" key={resource.id}>
-          <h3>{resource.title}</h3>
-          {/* <h5>by: {resource.User.username}</h5> */}
-        </div>
-      ))}
+    <div>
+      <h1>test</h1>
+      {authenticated && user ? authenticatedResources : publicResources}
     </div>
-  )
+  );
 }

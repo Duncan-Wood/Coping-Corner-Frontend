a101a3025fc6643f1f936ee9cf266af94255c02f
Yael Kaufman <ylkauf@gmail.com>
2023-03-20 16:07:01 -0700

    added LoggedHome buttons/functionality and some CSS

--- coping-frontend/src/components/Dashboard.jsx
+++ coping-frontend/src/components/Dashboard.jsx
@@ -1,7 +1,18 @@
+import {useNavigate} from 'react-router-dom'
+import {useContext, useState} from 'react'
+import {UserProvider} from '../UserProvider'
+
 export default function Dashboard(){
+    let navigate = useNavigate();
+    const {mood, setMood} = useContext(UserProvider)
+    const BackToMood = () => {
+        setMood(null);
+        navigate('/loggedhome')
+    }
     return(
         <div>
-            <h1>hi</h1>
+            <button id="go-back" onClick={BackToMood}> Go Back </button>
+            <h1>This is where the dashboard will go!</h1>
         </div>
     )
 }

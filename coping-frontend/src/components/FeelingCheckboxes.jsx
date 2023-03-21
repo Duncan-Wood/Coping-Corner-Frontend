30dd184e6da96f3a769def7f18c5e95b20c75d9a
Elliott Ventura <elliott.ventura26@gmail.com>
2023-03-21 13:38:40 -0500

    added to post component

--- coping-frontend/src/components/FeelingCheckboxes.jsx
+++ coping-frontend/src/components/FeelingCheckboxes.jsx
@@ -1,86 +1,107 @@
 export default function FeelingCheckboxes({handleCheckboxChange}){
+
     return(
         <div>
             <input type="checkbox" 
                    name="feelings" 
                    onChange={handleCheckboxChange} 
-                   value="angry"/> <span>angry</span>
+                   value="angry"/> 
+                   <span>angry</span>
             <input type="checkbox" 
                     name="feelings" 
                     onChange={handleCheckboxChange} 
-                    value="blah"  /> <span>blah</span>
+                    value="blah"  />
+                    <span>blah</span>
             <input type="checkbox" 
                     name="feelings" 
                     onChange={handleCheckboxChange} 
-                    value="fine"  /> <span>fine</span>
+                    value="fine"  /> 
+                    <span>fine</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="bad"  /> <span>bad</span>
+                    value="bad"  /> 
+                    <span>bad</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="afraid"  /> <span>afraid</span>
+                    value="afraid"  /> 
+                    <span>afraid</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="overwhelmed"  /> <span>overwhelmed</span>
+                    value="overwhelmed"  /> 
+                    <span>overwhelmed</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="under-stimulated"  /> <span>under-stimulated</span>
+                    value="under-stimulated"  /> 
+                    <span>under-stimulated</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="calm"  /> <span>calm</span>
+                    value="calm"  /> 
+                    <span>calm</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="lonely" /> <span>lonely</span>
+                    value="lonely" /> 
+                    <span>lonely</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="extremely irritable" /> <span>extremely irritable</span>
+                    value="extremely irritable" /> 
+                    <span>extremely irritable</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="tired" /> <span>tired</span>
+                    value="tired" /> 
+                    <span>tired</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="self-doubt" /> <span>self-doubt</span>
+                    value="self-doubt" /> 
+                    <span>self-doubt</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="good" /> <span>good</span>
+                    value="good" /> 
+                    <span>good</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="guilty" /> <span>guilty</span>
+                    value="guilty" /> 
+                    <span>guilty</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="disconnected" /> <span>disconnected</span>
+                    value="disconnected" />
+                    <span>disconnected</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="depressed" /> <span>depresssed</span>
+                    value="depressed" /> 
+                    <span>depresssed</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="sad" /> <span>sad</span>
+                    value="sad" />
+                    <span>sad</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="frustrated" /> <span>frustrated</span>
+                    value="frustrated" /> 
+                    <span>frustrated</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="happy" /> <span>happy</span>
+                    value="happy" /> 
+                    <span>happy</span>
             <input  type="checkbox" 
                     name="feelings"
                     onChange={handleCheckboxChange}
-                    value="totally distraught" /> <span>totally distraught</span>
+                    value="totally distraught" /> 
+                    <span>totally distraught</span>
         </div>
     )
 }
--- coping-frontend/src/components/Post.jsx
+++ coping-frontend/src/components/Post.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CreateResource } from "../Services/ResourceServices";
 import TypeCheckboxes from "./TypeCheckboxes";
 import FeelingCheckboxes from "./FeelingCheckboxes";
+import axios from "axios"
 
 export default function Post () {
     let navigate = useNavigate()
     const [selectedOption, setSelectedOption] = useState('');
-
-     const [formValues, setFormValues] = useState({
+    const [formValues, setFormValues] = useState({
         title: "",
         previewText: "",
         resourceContent: "",
@@ -18,55 +19,97 @@ export default function Post () {
         
      })
 
+     const checkboxArray = []
 
     const handleCheckboxChange = (e) => {
-        const { name, value, checked } = e.target;
-        const checkboxArray = formValues[name];
-        if (checked) {
-          setFormValues({
-            ...formValues,
-            [name]: [...checkboxArray, value]
-          });
-        } else {
-          setFormValues({
-            ...formValues,
-            [name]: checkboxArray.filter((checkbox) => checkbox !== value)
-          });
-        }
-      };
-     
+        // const { name, value, checked } = e.target;
 
+        if (e.target.checked) {
+          // checkboxArray.push(e.target.value)
+          let currentValue = e.target.value
+          setFormValues({
+            ...formValues,
+            [e.target.name]: [...checkboxArray, currentValue]
+          });
+        } 
+        }
+    
+     
+      
     const handleChange = (e) => {
         e.preventDefault()
         setFormValues({ ...formValues, [e.target.name]: e.target.value });
-        console.log(formValues);
-  };
+  }
 
-    function handleOptionChange(e) {
+    const handleOptionChange = (e) => {
         setSelectedOption(e.target.value);
-      }
+    }
 
     const options = [];
     for (let i = 1; i <= 60; i++) {
       options.push(<option key={i} value={i}>{i} minute{ i > 1 ? 's' : '' }</option>);
     }
 
-    //add handlesubmit to form and make function for that
-    //figure out how to tie in the chckbox values to this and how to make all this code work
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      await CreateResource({
+        title: formValues.title,
+        previewText: formValues.previewText,
+        resourceContent: formValues.resourceContent,
+        imageURL: formValues.imageURL,
+        resourceURL: formValues.resourceURL,
+        types: {TypeCheckboxes},
+        feelings: {FeelingCheckboxes}
+
+      });
+      setFormValues({
+        title: "",
+        previewText: "",
+        resourceContent: "",
+        imageURL: "",
+        resourceURL: "",
+        types: [],
+        feelings: []
+      });
+      navigate("/login");
+    };
+
+
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     try {
+    //       const token = localStorage.getItem("token");
+    //       const response = await axios.post(
+    //         "http://localhost:3001/api/resource/",
+    //         JSON.stringify(formValues),
+    //         {
+    //           headers: {
+    //             Authorization: `Bearer ${token}`,
+    //             "Content-Type": "application/json",
+    //           },
+    //         }
+    //       );
+    //       console.log(response.data);
+    //       navigate(`/posts/${response.data.id}`);
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   };
 
     return (
         <div>
             <h1>DRAFT A POST</h1>
-            <form className="resourceDraft" > 
+            <form className="resourceDraft" onSubmit={handleSubmit} > 
                 <div>
                     <label htmlFor="title">title</label>
                     <br></br>
                     <input 
-                    onChange={handleChange}
-                    value={formValues.title}
-                    name="title"
-                    type="text"
-                    placeholder="Type title here.." />
+                        onChange={handleChange}
+                        value={formValues.title}
+                        name="title"
+                        type="text"
+                        placeholder="Type title here.." />
                 </div>
 
             <TypeCheckboxes handleCheckboxChange={handleCheckboxChange}/>
@@ -84,7 +127,7 @@ export default function Post () {
                 </div>
 
                 <div>
-                    <label htmlFor="preview">preview text</label>
+                    <label htmlFor="previewText">preview text</label>
                     <br></br>
                     <input  onChange={handleChange}
                             value={formValues.previewText}
@@ -120,7 +163,7 @@ export default function Post () {
                             name="resourceURL" />
                 </div>
                 
-                <input type="submit" value="SHARE POST"/>
+                <input type="submit" value="SHARE POST" disabled={!formValues.title || !formValues.previewText || !formValues.resourceContent || !formValues.types || !formValues.feelings || !selectedOption }/>
 
 
             </form>

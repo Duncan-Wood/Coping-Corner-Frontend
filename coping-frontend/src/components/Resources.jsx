b14597be6470e8ff0370578c56b0a39fe2e08379
Duncan <duncanwoodpro@gmail.com>
2023-03-21 21:04:05 -0400

    searchbar trucking along

--- coping-frontend/src/components/Resources.jsx
+++ coping-frontend/src/components/Resources.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { GetResources } from "../Services/ResourceServices";
 import { useNavigate } from "react-router-dom";
 import { UserProvider } from "../UserProvider";
@@ -7,52 +7,64 @@ import SearchBar from "./SearchBar";
 export default function Resources() {
   const { user } = useContext(UserProvider);
   const { authenticated } = useContext(UserProvider);
+
   const { resources } = useContext(UserProvider);
   const { setResources } = useContext(UserProvider);
-  const { searchQuery } = useContext(UserProvider);
-  const { setSearchQuery } = useContext(UserProvider);
+
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedTypes, setSelectedTypes] = useState([]);
+  const [selectedFeelings, setSelectedFeelings] = useState([]);
+
   let navigate = useNavigate();
 
+  //Calls all resources on page load
   useEffect(() => {
-    const handleResources = async () => {
+    const handleGetResources = async () => {
       const results = await GetResources();
       setResources(results);
     };
-    handleResources();
+    handleGetResources();
   }, []);
 
-  const handleFilteredResources = () => {
-    return resources.filter((resource) => resource.user_id === 1);
+  //further if/else statements could be added to check if user has mood for dashboard
+  const filterResources = () => {
+    let filtered = resources;
+    //shows HQ resources if user is not logged in
+    if (!user && !authenticated) {
+      filtered = filtered.filter((resource) => resource.user_id === 1);
+    }
+
+    if (searchQuery) {
+      filtered = filtered.filter((resource) =>
+        resource.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    if (selectedTypes.length > 0) {
+      filtered = filtered.filter((resource) =>
+        selectedTypes.includes(resource.type)
+      );
+    }
+
+    if (selectedFeelings.length > 0) {
+      filtered = filtered.filter((resource) =>
+        selectedFeelings.some((feeling) => resource.feelings.includes(feeling))
+      );
+    }
+
+    return filtered;
   };
-  const filteredResources =
-    !user && !authenticated ? handleFilteredResources() : resources;
+  const handleSearch = (query, types, feelings) => {
+    setSearchQuery(query);
+    setSelectedTypes(types);
+    setSelectedFeelings(feelings);
+  };
 
-  let authenticatedResources;
-  if (user) {
-    authenticatedResources = (
-      <div className="user_feed">
-        <div className="search-bar-container">
-          <SearchBar />
-        </div>
-        <div className="logged_resources_header">
-          <h1>POPULAR RESOURCES</h1>
-        </div>
-        {filteredResources.map(
-          (resource) => (
-            console.log(filteredResources),
-            (
-              <div className="resource-card" key={resource.id}>
-                <h3>{resource.title}</h3>
-                <h5>by: {resource.User.username}</h5>
-              </div>
-            )
-          )
-        )}
-      </div>
-    );
-  }
+  const filteredResources = filterResources();
 
-  const publicResources = (
+  //What will display on Resources page if user is not logged in
+  const publicResourcesContent = (
     <div className="user_feed">
       <div className="unlogged-resources-header">
         <h1>POPULAR RESOURCES</h1>
@@ -79,10 +91,35 @@ export default function Resources() {
     </div>
   );
 
+  //What will display on Resources page if user is logged in
+  let authenticatedResources;
+  if (user) {
+    authenticatedResources = (
+      <div className="user_feed">
+        <div className="search-bar-container">
+          <SearchBar handleSearch={handleSearch}/>
+        </div>
+        <div className="logged_resources_header">
+          <h1>POPULAR RESOURCES</h1>
+        </div>
+        {filteredResources.map(
+          (resource) => (
+            console.log(filteredResources),
+            (
+              <div className="resource-card" key={resource.id}>
+                <h3>{resource.title}</h3>
+                <h5>by: {resource.User.username}</h5>
+              </div>
+            )
+          )
+        )}
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>test</h1>
-      {authenticated && user ? authenticatedResources : publicResources}
+      {authenticated && user ? authenticatedResources : publicResourcesContent}
     </div>
   );
 }

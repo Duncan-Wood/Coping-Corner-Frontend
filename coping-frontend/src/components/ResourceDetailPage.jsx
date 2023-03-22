ead3bab88c73ceb8bf586453983c79a5d6bbee53
Elliott Ventura <elliott.ventura26@gmail.com>
2023-03-22 15:17:29 -0500

    updated details component

--- coping-frontend/src/components/ResourceDetailPage.jsx
+++ coping-frontend/src/components/ResourceDetailPage.jsx
@@ -10,16 +10,40 @@ export default function ResourceDetailPage(){
         let selectedResource = async () => {
             const res = await axios.get('http://localhost:3001/api/resource/'+(id))
             setResource(res.data)
-            console.log(res.data)
+            console.log(res)
+            console.log(res.data.title)
         }
         selectedResource()
-
+        console.log(resource)
     },[id])
 
     return(
-        <div><h1>
-            "This is where the info bout the specific resource will go."
-            </h1>
-        </div>
+
+        <div>
+            {resource ? (
+        <div>
+
+=         <div>
+                 <h1>{resource.title.toUpperCase()}</h1>
+                 <h4>{resource.type.join(', ')}</h4>
+                 <h5>for when you're feeling:</h5><h5> {resource.feeling.join(' ')}</h5>
+                 <h5>by {resource.User.username}</h5>
+            </div>
+
+            <div>
+                <h2>{resource.content}</h2>
+                <h3>{resource.time_requirement}</h3>
+            </div>
+
+            <div>
+                <img src={resource.optional_image} style={{ height: '500px', width: '700px'  }} />
+            </div>
+        
+
+            <button>ADD TO MY TOOLKIT</button>
+         </div>
+         ) : ( <p>loading</p>)      }
+         </div>
+            
     )
 }

eb56153ace52a185ca702590592d8a1faf6b4a1a
Duncan <duncanwoodpro@gmail.com>
2023-03-23 11:26:59 -0400

    search bar works well enough

--- coping-frontend/src/components/SearchBar.jsx
+++ coping-frontend/src/components/SearchBar.jsx
@@ -5,64 +5,67 @@ export default function SearchBar({ filteredResources, setFilteredResources }) {
   const { resources } = useContext(UserProvider);
 
   const [searchQuery, setSearchQuery] = useState("");
-  const [selectedTypes, setSelectedTypes] = useState([]);
-  const [selectedFeelings, setSelectedFeelings] = useState([]);
 
-  const typeOptions = [
-    { value: "meditation", label: "Meditation" },
-    { value: "movement", label: "Movement" },
-    { value: "mind-body", label: "Mind-Body" },
-    { value: "distraction", label: "Distraction" },
-    { value: "grounding", label: "Grounding" },
-    { value: "affirmation", label: "Affirmation" },
-  ];
+  // const [selectedTypes, setSelectedTypes] = useState([]);
+  // const [selectedFeelings, setSelectedFeelings] = useState([]);
 
-  const feelingOptions = [
-    { value: "angry", label: "Angry" },
-    { value: "blah", label: "Blah" },
-    { value: "fine", label: "Fine" },
-    { value: "bad", label: "Bad" },
-    { value: "afraid", label: "Afraid" },
-    { value: "overwhelmed", label: "Overwhelmed" },
-    { value: "under-stimulated", label: "Under-Stimulated" },
-    { value: "calm", label: "Calm" },
-    { value: "lonely", label: "Lonely" },
-    { value: "guilty", label: "Guilty" },
-    { value: "disconnected", label: "Disconnected" },
-    { value: "tired", label: "Tired" },
-    { value: "good", label: "Good" },
-    { value: "sad", label: "Sad" },
-    { value: "self-doubt", label: "Self-Doubt" },
-    { value: "depressed", label: "Depressed" },
-    { value: "frustrated", label: "Frustrated" },
-    { value: "happy", label: "Happy" },
-    { value: "totally_distraught", label: "Totally Distraught" },
-    { value: "very_anxious", label: "Very Anxious" },
-  ];
+  // const typeOptions = [
+  //   { value: "meditation", label: "Meditation" },
+  //   { value: "movement", label: "Movement" },
+  //   { value: "mind-body", label: "Mind-Body" },
+  //   { value: "distraction", label: "Distraction" },
+  //   { value: "grounding", label: "Grounding" },
+  //   { value: "affirmation", label: "Affirmation" },
+  // ];
 
-  const handleTypeChange = (event) => {
-    const typeToAdd = event.target.value;
-    if (selectedTypes.includes(typeToAdd)) {
-      const selectedValues = selectedTypes.filter((type) => type !== typeToAdd);
-      setSelectedTypes(selectedValues);
-    } else {
-      const selectedValues = [...selectedTypes, typeToAdd];
-      setSelectedTypes(selectedValues);
-    }
-  };
+  // const feelingOptions = [
+  //   { value: "angry", label: "Angry" },
+  //   { value: "blah", label: "Blah" },
+  //   { value: "fine", label: "Fine" },
+  //   { value: "bad", label: "Bad" },
+  //   { value: "afraid", label: "Afraid" },
+  //   { value: "overwhelmed", label: "Overwhelmed" },
+  //   { value: "under-stimulated", label: "Under-Stimulated" },
+  //   { value: "calm", label: "Calm" },
+  //   { value: "lonely", label: "Lonely" },
+  //   { value: "guilty", label: "Guilty" },
+  //   { value: "disconnected", label: "Disconnected" },
+  //   { value: "tired", label: "Tired" },
+  //   { value: "good", label: "Good" },
+  //   { value: "sad", label: "Sad" },
+  //   { value: "self-doubt", label: "Self-Doubt" },
+  //   { value: "depressed", label: "Depressed" },
+  //   { value: "frustrated", label: "Frustrated" },
+  //   { value: "happy", label: "Happy" },
+  //   { value: "totally_distraught", label: "Totally Distraught" },
+  //   { value: "very_anxious", label: "Very Anxious" },
+  // ];
 
-  const handleFeelingChange = (event) => {
-    const feelingToAdd = event.target.value;
-    if (selectedFeelings.includes(feelingToAdd)) {
-      const selectedValues = selectedFeelings.filter(
-        (type) => type !== feelingToAdd
-      );
-      setSelectedFeelings(selectedValues);
-    } else {
-      const selectedValues = [...selectedFeelings, feelingToAdd];
-      setSelectedFeelings(selectedValues);
-    }
-  };
+  // const handleTypeChange = (event) => {
+  //   const typeToAdd = event.target.value;
+  //   if (selectedTypes.includes(typeToAdd)) {
+  //     const selectedValues = selectedTypes.filter((type) => type !== typeToAdd);
+  //     setSelectedTypes(selectedValues);
+  //   } else {
+  //     const selectedValues = [...selectedTypes, typeToAdd];
+  //     setSelectedTypes(selectedValues);
+  //     console.log(selectedTypes)
+  //   }
+  // };
+
+  // const handleFeelingChange = (event) => {
+  //   const feelingToAdd = event.target.value;
+  //   if (selectedFeelings.includes(feelingToAdd)) {
+  //     const selectedValues = selectedFeelings.filter(
+  //       (type) => type !== feelingToAdd
+  //     );
+  //     setSelectedFeelings(selectedValues);
+  //   } else {
+  //     const selectedValues = [...selectedFeelings, feelingToAdd];
+  //     setSelectedFeelings(selectedValues);
+  //   }
+  // };
+
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
@@ -71,21 +74,19 @@ export default function SearchBar({ filteredResources, setFilteredResources }) {
     event.preventDefault();
     const resourceFilter = resources.filter(
       (resource) =>
-      resource.title.includes('Aromatherapy')
-        // resource.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        // resource.content.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        // resource.type.some(selectedTypes) ||
-        // resource.feelings.some(selectedFeelings) 
+        resource.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        resource.content.toLowerCase().includes(searchQuery.toLowerCase())
+        // resource.feeling ? resource.feeling.includes(selectedFeelings) : null
+        // resource.type.includes(selectedTypes)
+        // resource.feeling.some(selectedFeelings) 
     );
     setFilteredResources(resourceFilter);
-    // console.log(resources);
-
     console.log(filteredResources);
   };
 
   return (
     <form className="resourceDraft" onSubmit={handleSubmit}>
-      <div>
+      {/* <div>
         <label htmlFor="type">Type:</label>
         <div className="typeCheckbox">
           {typeOptions.map((option) => (
@@ -121,8 +122,7 @@ export default function SearchBar({ filteredResources, setFilteredResources }) {
             </div>
           ))}
         </div>
-      </div>
-
+      </div> */}
       <div>
         <input
           type="text"

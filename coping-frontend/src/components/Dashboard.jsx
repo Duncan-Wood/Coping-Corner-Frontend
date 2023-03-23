93843108d53decd2a1f9992db17dc4b9564694a5
Duncan <duncanwoodpro@gmail.com>
2023-03-23 12:18:35 -0400

    working on dashboard

--- coping-frontend/src/components/Dashboard.jsx
+++ coping-frontend/src/components/Dashboard.jsx
@@ -5,21 +5,14 @@ import SearchBar from "./SearchBar";
 
 export default function Dashboard() {
   const { resources } = useContext(UserProvider);
-  const [filteredResources, setFilteredResources] = useState([])
+  const [filteredResources, setFilteredResources] = useState([]);
 
   const { mood, setMood } = useContext(UserProvider);
-  const [moodResources, setmoodResources] = useState([])
 
   let navigate = useNavigate();
   const goBack = () => {
     navigate(-1);
   };
-
-  for (let i = 0; i < resources.length; i++) {
-    if (resources[i].feeling.includes(mood)) {
-      setmoodResources(...moodResources, resources[i])
-    }
-  }
 
   const showResource = (index) => {
     navigate(`/resources/detail/${index}`);
@@ -30,45 +23,53 @@ export default function Dashboard() {
       <button id="go-back" onClick={goBack}>
         Go Back
       </button>
-      
-        <div className="search-bar-container">
-          <SearchBar filteredResources={filteredResources} setFilteredResources={setFilteredResources} />
-        </div>
-        <Link to ='/post'><button className="dashboard-add-post">+ ADD A NEW POST</button></Link>
-        <Link to ='/toolkit'><button className="dashboard-view-toolkit">VIEW TOOLKIT</button></Link>
-     
-      <div className='dashboard-grid'>
-        <h2 className = "suggestions">Here are some suggestions for tools to use when you're feeling:</h2>
-      
-          {resources.map(
-            (resource) => (
-              console.log(resources),
-              console.log(mood),
-              (
 
-                <div className="resource-card"
-                  key={resource.id}
-                  onClick={() => showResource(resource.id)}
-                >
-                  <h1 className = "loggedTitle"> {resource.title} </h1>
-                  <span className="category-for-card">TYPE</span> <h5 className="resource-type">{resource.type.join(", ")} </h5>
-                  <span className="category-for-card">FOR WHEN YOU'RE FEELING</span><h5 className="resource-feeling">{resource.feeling.join(", ")} </h5>
-                  <h2 className="preview-text"> {resource.preview_text} </h2>
-                  <div className="container-for-image">
-                    <img className="small-img-card" src={resource.optional_image} />
-                  </div>
-                  <h5> <span className="category-for-card">TIME REQUIREMENT</span>{resource.time_requirement} minutes </h5>
-                  <div className="likes-container">
-                    {/* <h6>star</h6> */}
-                    {/* <button>like</button> */}
-                    <h6>{resource.likes} others love this!</h6>
-                  </div>
-                </div>
-        
-        )
-        )
-        )}
+      <div className="search-bar-container">
+        <SearchBar
+          filteredResources={filteredResources}
+          setFilteredResources={setFilteredResources}
+        />
       </div>
+      <Link to="/post">
+        <button className="dashboard-add-post">+ ADD A NEW POST</button>
+      </Link>
+      <Link to="/toolkit">
+        <button className="dashboard-view-toolkit">VIEW TOOLKIT</button>
+      </Link>
 
-  </div>)}
+      <div className="dashboard-grid">
+        <h2 className="suggestions">
+          Here are some suggestions for tools to use when you're feeling {mood ?  <span>{mood}</span> : <span>anything</span>}:
+        </h2>
 
+        {resources.map((resource) => (
+          <div
+            className="resource-card"
+            key={resource.id}
+            onClick={() => showResource(resource.id)}
+          >
+            <h1 className="loggedTitle"> {resource.title} </h1>
+            <span className="category-for-card">TYPE</span>{" "}
+            <h5 className="resource-type">{resource.type.join(", ")} </h5>
+            <span className="category-for-card">FOR WHEN YOU'RE FEELING</span>
+            <h5 className="resource-feeling">{resource.feeling.join(", ")} </h5>
+            <h2 className="preview-text"> {resource.preview_text} </h2>
+            <div className="container-for-image">
+              <img className="small-img-card" src={resource.optional_image} />
+            </div>
+            <h5>
+              {" "}
+              <span className="category-for-card">TIME REQUIREMENT</span>
+              {resource.time_requirement} minutes{" "}
+            </h5>
+            <div className="likes-container">
+              {/* <h6>star</h6> */}
+              {/* <button>like</button> */}
+              <h6>{resource.likes} others love this!</h6>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- coping-frontend/src/components/Resources.jsx
+++ coping-frontend/src/components/Resources.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserProvider } from "../UserProvider";
-// import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar";
 
 export default function Resources() {
   const { user } = useContext(UserProvider);
   const { authenticated } = useContext(UserProvider);
 
   const { resources } = useContext(UserProvider);
+  const [filteredResources, setFilteredResources] = useState([])
 
   let navigate = useNavigate();
   const showResource = (index) => {
@@ -15,51 +16,48 @@ export default function Resources() {
   };
 
   //further if/else statements could be added to check if user has mood for dashboard
-  const showPublicResources = () => {
+  const filterResources = () => {
     let filtered = resources;
     //shows HQ resources if user is not logged in
     if (!user && !authenticated) {
       filtered = filtered.filter((resource) => resource.user_id === 1);
+    } else if (filteredResources !== []) {
+      filtered = resources
+    } else if (filteredResources) {
+      filtered = filteredResources
     }
     return filtered;
   };
 
-  const publicResources = showPublicResources();
+
+  const publicResources = filterResources();
 
   //What will display on Resources page if user is not logged in
   const publicResourcesContent = (
-    <div className="user_feed_loggedout">
+    <div className="user_feed">
       <div className="unlogged-resources-header">
         <h1>POPULAR RESOURCES</h1>
         <h2>BY COPING CORNER HQ</h2>
       </div>
       <div className="please-register">
-        <h5 className="viewmore">
+        <h5>
           TO VIEW OUR WHOLE LIBRARY OF RESOURCES, PLEASE REGISTER A
-          <span className="freeaccount"> FREE ACCOUNT </span>
+          <span style={{ color: "cyan" }}> FREE ACCOUNT </span>
           OR LOG IN NOW!
         </h5>
       </div>
-      <div className = "loggedout-resource-grid">
       {publicResources.map((resource) => (
-        // console.log(publicResources)
-        <div className="resource-card" key={resource.id} onClick={() => showResource(resource.id)}
+        <div
+          className="resource-card"
+          key={resource.id}
+          onClick={() => showResource(resource.id)}
         >
-                <h1> {resource.title} </h1>
-                <span className="category-for-card">TYPE</span> <h5 className="resource-type">{resource.type.join(", ")} </h5>
-                <span className="category-for-card">FOR WHEN YOU'RE FEELING</span><h5 className="resource-feeling">{resource.feeling.join(", ")} </h5>
-                <h2 className="preview-text"> {resource.preview_text} </h2>
-                <div className="container-for-image">
-                  <img className="small-img-card" src={resource.optional_image} />
-                </div>
-                <h5> <span className="category-for-card">TIME REQUIREMENT</span>{resource.time_requirement} minutes </h5>
-                <div className="likes-container">
-                <h5>{resource.likes} people love this!</h5>
-                </div>
-        
-      </div>))}
-      </div></div>
-)
+          <h3>{resource.title}</h3>
+          <h5>by: {resource.User.username}</h5>
+        </div>
+      ))}
+    </div>
+  );
 
   //What will display on Resources page if user is logged in
   let authenticatedResources;
@@ -67,31 +65,25 @@ export default function Resources() {
     authenticatedResources = (
       <div className="user_feed">
         <div className="search-bar-container">
-          {/* <SearchBar handleSearch={handleSearch}/> */}
-          <h4>Search Bar Goes Here</h4>
+        <SearchBar filteredResources={filteredResources} setFilteredResources={setFilteredResources}/>          <h4>Search Bar Goes Here</h4>
         </div>
         <div className="logged_resources_header">
-          <h1>RESOURCES</h1>
+          <h1>POPULAR RESOURCES</h1>
         </div>
-        <div className = "loggedin-resource-grid">
         {publicResources.map((resource) => (
-          <div className="resource-card" key={resource.id} onClick={() => showResource(resource.id)}
+          <div
+            className="resource-card"
+            key={resource.id}
+            onClick={() => showResource(resource.id)}
           >
-                  <h1 className = "loggedTitle"> {resource.title} </h1>
-                  <span className="category-for-card">TYPE</span> <h5 className="resource-type">{resource.type.join(", ")} </h5>
-                  <span className="category-for-card">FOR WHEN YOU'RE FEELING</span><h5 className="resource-feeling">{resource.feeling.join(", ")} </h5>
-                  <h2 className="preview-text"> {resource.preview_text} </h2>
-                  <div className="container-for-image">
-                    <img className="small-img-card" src={resource.optional_image} />
-                  </div>
-                  <h5> <span className="category-for-card">TIME REQUIREMENT</span>{resource.time_requirement} minutes </h5>
-                  <div className="likes-container">
-                    <h5>{resource.likes} people love this!</h5>
-                  </div>
-          
-        </div>))}
-        </div></div>
-  )}
+            <h3>{resource.title}</h3>
+            <h5>by: {resource.User.username}</h5>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div>
       {authenticated && user ? authenticatedResources : publicResourcesContent}
